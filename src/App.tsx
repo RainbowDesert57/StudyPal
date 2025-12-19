e48f6b59b68b1331674ca64d596f51df08ee29df
Tanish <tanishj1711@gmail.com>
2025-12-19 09:18:07 +0530

    Feat(Pointer): Added click animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import './App.css'
 import './components/menu-bar'
 import MenuBar from './components/menu-bar'
 import HomePage from './pages/home'
+import ClickSpark from './components/reactBits/click/ClickSpark';
 
 function App() {
   return (
     <>
+      <ClickSpark
+        sparkColor='#000000'
+        sparkSize={15}
+        sparkRadius={25}
+        sparkCount={8}
+        duration={400}
+      >
       <MenuBar />
       <HomePage />
+      </ClickSpark>
     </>
   )
 }
